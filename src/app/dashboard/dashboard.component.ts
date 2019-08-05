e92b2ab9929ba9a18b5c45cf181581250cc5afc5
Dan Martin <danmartindev@gmail.com>
2019-08-04 22:35:47 -0400

    experiemnting with dragging locations

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,12 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  //@ViewChild('myLock', {static: false}) myLock: ElementRef;
+
+  elements = [
+    {name: "blech"},
+  ];
+
+  myelems;
+
+
+  toShift;
+  constructor(private elem: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    // you'll get your through 'elements' below code
+    this.myelems = this.elem.nativeElement.querySelectorAll('.snapper');
+    console.log(this.myelems);
   }
 
   events: string[] = [];
   opened: boolean;
 
+  createEle(){
+    this.elements.push({name:"newBtn"});
+  }
+
+  getLoc(event){
+    this.toShift=event.target;
+    console.log(event.target.getBoundingClientRect());
+    // this.myelems.forEach(el => {
+    //   console.log(el.offsetX);
+    // });
+    //console.log(this.myLock);
+
+    //offsetLeft 8
+    //offsetHeight 18
+    //offsetTop: 635
+    //offsetWidth: 302
+  }
+
+  shift(){
+    console.log(this.toShift.getBoundingClientRect().x);
+    console.log("shifting");
+    this.toShift.getBoundingClientRect().x = this.toShift.getBoundingClientRect().x + 50; 
+    console.log(this.toShift.getBoundingClientRect().x);
+
+  }
 }
