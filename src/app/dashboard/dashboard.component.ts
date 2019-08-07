183f4ad5346af8a4fe0065aa81b76a7793254ea8
Dan Martin <danmartindev@gmail.com>
2019-08-06 21:59:41 -0400

    updated to marge list

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,53 +8,34 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  //@ViewChild('myLock', {static: false}) myLock: ElementRef;
-
-  elements = [
-    {name: "blech"},
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
   ];
 
-  myelems;
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
 
-
-  toShift;
-  constructor(private elem: ElementRef) { }
-
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+  
   ngOnInit() {
   }
 
-  ngAfterViewInit(){
-    // you'll get your through 'elements' below code
-    this.myelems = this.elem.nativeElement.querySelectorAll('.snapper');
-    console.log(this.myelems);
-  }
-
-  events: string[] = [];
-  opened: boolean;
-
-  createEle(){
-    this.elements.push({name:"newBtn"});
-  }
-
-  getLoc(event){
-    this.toShift=event.target;
-    console.log(event.target.getBoundingClientRect());
-    // this.myelems.forEach(el => {
-    //   console.log(el.offsetX);
-    // });
-    //console.log(this.myLock);
-
-    //offsetLeft 8
-    //offsetHeight 18
-    //offsetTop: 635
-    //offsetWidth: 302
-  }
-
-  shift(){
-    console.log(this.toShift.getBoundingClientRect().x);
-    console.log("shifting");
-    this.toShift.getBoundingClientRect().x = this.toShift.getBoundingClientRect().x + 50; 
-    console.log(this.toShift.getBoundingClientRect().x);
-
-  }
+ 
 }
