391a60f7bfffdc6c7b890ea573fb3082fa3fb2ff
Dan Martin <danmartindev@gmail.com>
2019-08-08 14:29:57 -0400

    imported app-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { AppListComponent } from './app-list/app-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AppListComponent
   ],
   imports: [
     BrowserModule,
