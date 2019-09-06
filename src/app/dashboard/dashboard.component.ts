fd7b68658f574cc82d15da3a14cb7d1080406ff5
Dan Martin <danmartindev@gmail.com>
2019-09-05 23:45:52 -0400

    updated for new directive handling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 import { TodoComponent } from '../todo/todo.component';
+import { AppListComponent } from '../app-list/app-list.component';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,34 +13,23 @@ import { TodoComponent } from '../todo/todo.component';
 
 export class DashboardComponent implements OnInit {
 
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi'
-  ];
-
   elements = [
     'list',
     'todo'
   ];
 
-  myEl = TodoComponent;
+  elTest = [
+    TodoComponent,
+    AppListComponent
+  ];
 
-  // indexes = [
-  //   {index: 1, component: },
-  //   {}
-  // ];
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-    moveItemInArray(this.elements, event.previousIndex, event.currentIndex);
+    // moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    // moveItemInArray(this.elements, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.elTest, event.previousIndex, event.currentIndex);
 
-    console.log(this.elements);
+    console.log(this.elTest);
   }
 
   ngOnInit() {
