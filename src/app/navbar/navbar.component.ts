e57b17fb1f287c3deef9c41747c9476b0c088634
Dan Martin <danmartindev@gmail.com>
2019-10-02 22:50:07 -0400

    window scroll event

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  currentHeight;
+  barHeight = '64px';
+
+  @HostListener('window:scroll', ['$event'])
+  doSomething(event){
+    console.log("Scroll Event " + window.pageYOffset);
+    console.log("CH " + this.currentHeight);
+    if(window.pageYOffset >= 100){
+      this.barHeight = '5px';
+    }
+  }
+  
   constructor() { }
 
   ngOnInit() {
