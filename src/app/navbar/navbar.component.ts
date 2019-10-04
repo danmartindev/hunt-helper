f5501e9231f141502704856378be78e6289c776a
Dan Martin <danmartindev@gmail.com>
2019-10-03 23:09:41 -0400

    added functions for mouse placement and scoll

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,22 +1,41 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { navAnim } from '../animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    navAnim
+  ]
 })
 export class NavbarComponent implements OnInit {
 
-  currentHeight;
-  barHeight = '64px';
+  scroll = false;
+  navState = '*';
+  timeout;
+  currentYOff = 0;
+
+  @HostListener('document:mousemove', ['$event']) 
+  showNav(e) {
+    if(e.clientY < 65) {
+      this.navState = '*';
+    }
+  }
 
   @HostListener('window:scroll', ['$event'])
-  doSomething(event){
-    console.log("Scroll Event " + window.pageYOffset);
-    console.log("CH " + this.currentHeight);
-    if(window.pageYOffset >= 100){
-      this.barHeight = '5px';
-    }
+  hideNav(event){
+    console.log("scrolling");
+    this.scroll = true;
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      console.log("done scrolling");
+      if(window.pageYOffset > 100 && Math.abs(window.pageYOffset - this.currentYOff) >= 100){
+        this.navState = 'hidden';
+      } else {
+        this.navState = '*';
+      }
+    }, 10);
   }
   
   constructor() { }
